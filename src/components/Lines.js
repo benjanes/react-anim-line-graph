de8faad31ac1741e04404f84792eb3f0177ba75a
Ben Janes <brjanes@gmail.com>
2016-06-03 16:29:07 -0400

    Add path rendering to Lines component

--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+const axisStyle = {
+  stroke: '#000',
+  strokeLinecap: 'round'
+};
+
 export default class Lines extends Component {
 
+  _renderLine(line) {
+    return <path key={line.key} stroke="#FF0000" fill="none" d={line.d} />
+  }
+
   render() {
-    <div>
-      
-    </div>
+    return (
+      <g>
+        <g id="anim-line-graph--lines">
+          {this.props.lines.map(this._renderLine)}
+        </g>
+        <g>
+          <path style={axisStyle} d="M 0 100 l 400 0" />
+          <path style={axisStyle} d="M 0 0 l 0 100" />
+        </g>
+      </g>
+    )
   }
 }
