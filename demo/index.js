ad7cbd8503dd2b2e4bd10a7e944ec9f6272ec481
Ben Janes <brjanes@gmail.com>
2016-06-03 12:03:57 -0400

    Add demo for module

---
+++ demo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Graph } from '../dist';
+
+const rootNode = document.getElementById('root');
+
+ReactDOM.render(<Graph />, rootNode);
