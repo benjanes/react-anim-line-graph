90ab72b4427d8cffe0efd69b5d0b15b8abc6cf8f
Ben Janes <brjanes@gmail.com>
2016-06-03 16:26:30 -0400

    Add mock data, add data as prop to graph

--- demo.js
+++ demo.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { AnimLineGraph, AnimLineGraphXAxis, AnimLineGraphYAxis } from './dist';
 
+const myData = {
+  line1 : [ 20, 40, 25, 60, 23 ],
+};
+
 ReactDOM.render(
-  <AnimLineGraph>
+  <AnimLineGraph data={myData} >
     <AnimLineGraphXAxis />
     <AnimLineGraphYAxis />
   </AnimLineGraph>, 
